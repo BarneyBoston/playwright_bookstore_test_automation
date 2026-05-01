c19610bb4b0bb8425ed3402d74cf7f2a31d2d926
Bartosz <koston178@gmail.com>
2026-05-01 10:56:30 +0200

    added configuration to run docker-compose before test starts

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:8080',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -72,9 +72,16 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    // command to clear and un your server
+    command: 'powershell -Command "try { $conn = Get-NetTCPConnection -LocalPort 3306 -ErrorAction Stop; Stop-Process -Id $conn.OwningProcess -Force } catch {}; docker compose up -d"',
+    cwd: __dirname,
+    // url Playwright waits before tests start
+    url: 'http://localhost:8080',
+    // If containers are already running, Playwright won't restart them (saves time locally)
+    reuseExistingServer: !process.env.CI,
+    // time in ms (here 2 minutes) for the Docker environment to fully start
+    timeout: 120 * 1000,
+  },
 });
+
